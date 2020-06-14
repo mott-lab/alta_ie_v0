62c063559874a86f2c99a8140f2eefbcc9056f60
Matt Gottsacker <mgottsacker34@gmail.com>
2020-06-14 18:44:37 -0500

    push build to git

---
+++ build/precache-manifest.a45268b8d47228ec9ba4483387cbf135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d5244dece37a8e46d5c72022f828db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90dfb77e48561c0cf703",
+    "url": "/static/css/main.57afe34b.chunk.css"
+  },
+  {
+    "revision": "219e8ca67ab5fbc8a4f5",
+    "url": "/static/js/2.fa669d4d.chunk.js"
+  },
+  {
+    "revision": "a5a226a48cca6a60e9e9dd1ee1b6d274",
+    "url": "/static/js/2.fa669d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90dfb77e48561c0cf703",
+    "url": "/static/js/main.26426676.chunk.js"
+  },
+  {
+    "revision": "d6a522c245ec254dd1bc",
+    "url": "/static/js/runtime-main.1235f9f7.js"
+  }
+]);
