6ef5360115f4d891c77e9b79e90db94ffdb813ea
Matt Gottsacker <mgottsacker34@gmail.com>
2020-06-14 18:50:00 -0500

    change root on build

---
+++ build/precache-manifest.81c0effc33239c9b5dfa818b75cb006e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b0fa382384bf100c2e5ab25bb99887",
+    "url": "/alta_ie_v0/index.html"
+  },
+  {
+    "revision": "ef1ebe7b3fcdf05ffd1e",
+    "url": "/alta_ie_v0/static/css/main.57afe34b.chunk.css"
+  },
+  {
+    "revision": "219e8ca67ab5fbc8a4f5",
+    "url": "/alta_ie_v0/static/js/2.fa669d4d.chunk.js"
+  },
+  {
+    "revision": "a5a226a48cca6a60e9e9dd1ee1b6d274",
+    "url": "/alta_ie_v0/static/js/2.fa669d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef1ebe7b3fcdf05ffd1e",
+    "url": "/alta_ie_v0/static/js/main.b9430dc4.chunk.js"
+  },
+  {
+    "revision": "81860d2ea82506e9a44d",
+    "url": "/alta_ie_v0/static/js/runtime-main.e2dbcf35.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a45268b8d47228ec9ba4483387cbf135.js"
+  "/alta_ie_v0/precache-manifest.81c0effc33239c9b5dfa818b75cb006e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alta_ie_v0/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
