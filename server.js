c53a10cb855959300766fb1d45151a57b1a47ff0
Matt Gottsacker <mgottsacker34@gmail.com>
2020-06-15 13:44:37 -0500

    simple express server added

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/alta_ie_v0/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+});
+
+app.listen(5000);
